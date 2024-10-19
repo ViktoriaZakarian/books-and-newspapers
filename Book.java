e42c9025535c0585cdbedb21a61709aa1e39cb66
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-10-19 12:37:48 +0300

    Initial commit

---
+++ Book.java
@@ -0,0 +1,97 @@
+package task0410;
+
+public class Book {
+    private String name;
+    private String author;
+    private String publishing;
+    private int yearOfPublication;
+    private int pages;
+    private int price;
+    private String typeOfBinding;
+
+    public Book(String name, String author, String publishing, int yearOfPublication, int pages, int price, String typeOfBinding) {
+        this.name = name;
+        this.author = author;
+        this.publishing = publishing;
+        this.yearOfPublication = yearOfPublication;
+        this.pages = pages;
+        this.price = price;
+        this.typeOfBinding = typeOfBinding;
+    }
+
+    public Book() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublishing() {
+        return publishing;
+    }
+
+    public void setPublishing(String publishing) {
+        this.publishing = publishing;
+    }
+
+    public int getYearOfPublication() {
+        return yearOfPublication;
+    }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getTypeOfBinding() {
+        return typeOfBinding;
+    }
+
+    public void setTypeOfBinding(String typeOfBinding) {
+        this.typeOfBinding = typeOfBinding;
+    }
+
+    void print() {
+        System.out.println(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", publishing='" + publishing + '\'' +
+                ", yearOfPublication=" + yearOfPublication +
+                ", pages=" + pages +
+                ", price=" + price +
+                ", typeOfBinding='" + typeOfBinding + '\'' +
+                '}';
+    }
+}
---
+++ Main.java
@@ -0,0 +1,49 @@
+package task0410;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Book> list = new ArrayList<>();
+        list.add(new Book("Harry Potter and Chamber of Secrets", "J. Rowling", "ROSMAN", 2000, 823, 40, "hardcover"));
+        list.add(new Book("Harry Potter and goblet of fire", "J. Rowling", "ROSMAN", 2002, 858, 45, "hardcover"));
+        list.add(new Book("Fight Club", "Ch.Palahniuk", "ANTALOGIA", 2018, 160, 15, "paperback"));
+        list.add(new Book("Java: guide for beginners", "G.Shmidt", "Dialektica", 2019, 816, 100, "hardcover"));
+        list.add(new Book("Java: complete guide", "G.Shmidt", "Dialektica", 2018, 1488, 130, "hardcover"));
+        System.out.println("Книги Дж.Роулинг: ");
+        for (Book book : list) {
+            if (Objects.equals(book.getAuthor(), "J. Rowling")) {
+                book.print();
+            }
+        }
+        System.out.println();
+        System.out.println("Книги издательства АНТАЛОГИЯ:");
+        for (Book book : list) {
+            if (Objects.equals(book.getPublishing(), "ANTALOGIA")) {
+                book.print();
+            }
+        }
+        System.out.println();
+        System.out.println("Книги, выпущенные в 2018 году:");
+        for (Book book : list) {
+            if (Objects.equals(book.getYearOfPublication(), 2018)) {
+                book.print();
+            }
+        }
+        System.out.println();
+        System.out.println("Книги в твердом переплете:");
+        for (Book book : list) {
+            if (Objects.equals(book.getTypeOfBinding(), "hardcover")) {
+                book.print();
+            }
+        }
+        System.out.println();
+        System.out.println("Книги дороже 150 рублей:");
+        for (Book book : list) {
+            if (book.getPrice() > 150) {
+                book.print();
+            }
+        }
+    }
+}
